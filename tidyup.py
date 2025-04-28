c8d92d5ba0720d88ec5161cb58b1f26f95ab3e85
Fambot_RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2025-04-28 16:02:50 +0900

    Update tidyup.py

--- tidyup.py
+++ tidyup.py
@@ -1,19 +1,23 @@
 import rospy
-import re
+import re, json, requests
 import cv2, os, time
+import mimetypes
 from loguru import logger
+from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty
 from LemonEngine.sensors import Camera
 from LemonEngine.hardwares.respeaker import Respeaker
 from LemonEngine.hardwares.chassis import Chassis
 from RobotChassis import RobotChassis
 
-POINT_DINING_ROOM = (-0.913,0.257,0.001) ##
+POINT_1 = (3.498, 3.339, -1.664)
+
+
 
 PROMPT = """
 # Instruction
-Analyze the input image. Detect distinct objects and classify them using the `Object List` below. 
-If an object isn't listed, use category `Unknown`. 
+Analyze the input image. Detect distinct objects and try your best to classify them using the `Object List` below. 
+If an object isn't listed, use category `Unknown`. Be careful not to leave any items behide
 Output *only* a JSON list containing objects with keys `"object"` and `"category"`. List each object type only once.
 
 # Object List
@@ -28,6 +32,8 @@ Output *only* a JSON list containing objects with keys `"object"` and `"category
 | 7  | Dice          | Task Item    |
 | 8  | Light Bulb    | Task Item    |
 | 9  | Block         | Task Item    |
+
+* Note: The bulb will be packed in a box
 
 # Example Output
 ```json
@@ -102,70 +108,90 @@ def main():
     cx, cy = width // 2, height // 2
     rate = rospy.Rate(20)
 
-    while not rospy.is_shutdown():
-        frame = cam1.get_frame()
-        depth_frame = cam2.get_frame()
-        depth_frame = cv2.resize(depth_frame, (width, height))
-        depth = depth_frame[cy, cx]
+    # while not rospy.is_shutdown():
+    #     frame = cam1.get_frame()
+    #     depth_frame = cam2.get_frame()
+    #     depth_frame = cv2.resize(depth_frame, (width, height))
+    #     depth = depth_frame[cy, cx]
 
-        frame = cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-        frame = cv2.putText(frame, f"{depth}", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        if depth > 2000:
-            respeaker.say("The door is opened")
+    #     frame = cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+    #     frame = cv2.putText(frame, f"{depth}", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    #     if depth > 2000:
+    #         respeaker.say("The door is opened")
 
-            chassis_move.set_linear(0.25)
-            time.sleep(5)
+    #         for i in range(10):
+    #             move(0.25, 0)
+    #             time.sleep(0.1)
+            
+    #         move(0, 0)
 
-            chassis_move.stop_moving()
-            break
+    #         break
 
-        cv2.imshow("depth", depth_frame)
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    #     cv2.imshow("depth", depth_frame)
+    #     cv2.imshow("frame", frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
 
+    # clear_costmaps
 
-    # navigate
-    chassis.move_to(*POINT_DINING_ROOM)
-    while not rospy.is_shutdown():
-        # 4. Get the chassis status.
-        rate = rospy.Rate(20)
-        code = chassis.status_code
-        text = chassis.status_text
-        logger.debug(f"Nav: {text}")
-        if code == 3:
-            break
+    # # navigate
+    # chassis.move_to(*POINT_1)
+    # while not rospy.is_shutdown():
+    #     # 4. Get the chassis status.
+    #     rate = rospy.Rate(20)
+    #     code = chassis.status_code
+    #     text = chassis.status_text
+    #     logger.debug(f"Nav: {text}, Code: {code}")
+    #     if code == 3:
+    #         break
 
-    clear_costmaps
+    # clear_costmaps
     
     
     respeaker.say("I am recognizing objects")
     json_object = None
     
     while True:
-	    logger.info("Asking Gemini for objects")
-	    frame = cam1.get_frame()
-	    cv2.imwrite("./image.jpg", frame)
-	    text = generate_content(PROMPT, "./image.jpg")
-	    text = text.replace("\n", "")
-	    text = text.replace("\r", "")
-	    print("Gemini Res", text)
-	
-		pattern = r"```json\s(.*?)\s```"  # Corrected regex pattern
-		match = re.search(pattern, text)
+        logger.info("Asking Gemini for objects")
+        frame = cam1.get_frame()
+        cv2.imwrite("./image.jpg", frame)
+        text = generate_content(PROMPT, "./image.jpg").get('generated_text')
+        if text is None:
+            respeaker.say("Failed")
+            continue
+        text = text.replace("\n", "")
+        text = text.replace("\r", "")
+        print("Gemini Res", text)
 
-		if match:
-		    extracted_json = match.group(1)  # Extract the content inside ```json ... ```
-    		json_object = json.loads(json_string)
-		    print(extracted_json)
-			break
+        pattern = r"```json(.*?)```"  # Corrected regex pattern
+        match = re.search(pattern, text)
+
+        if match:
+            json_string = match.group(1)  # Extract the content inside ```json ... ```
+            json_object = json.loads(json_string)
+            print(json_string)
+            break
 	
 	
     respeaker.say("I see")
-    for i in json_object:
-    	print(i)
-    	respeaker.say(f"Help me take {i["object"]}")
-    	t.sleep(5)
+    for a_object in json_object:
+        print(a_object)
+        respeaker.say("Help me take " + a_object["object"])
+        time.sleep(5)
+
+        for _ in range(25):
+            move(0, 1.0)
+            time.sleep(0.1)
+        time.sleep(1)
+
+        respeaker.say("Putting to " + a_object["category"])
+
+
+        for _ in range(25):
+            move(0, -1.0)
+            time.sleep(0.1)
+        time.sleep(1)
+
     
 
     
