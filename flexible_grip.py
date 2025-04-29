37115dce06d1d9ebcd66f64c91153bf26bda319e
IsaacMak <95964568+IsaacMak2009@users.noreply.github.com>
2025-04-29 18:50:47 +0800

    Update flexible_grip.py

--- flexible_grip.py
+++ flexible_grip.py
@@ -1,16 +1,24 @@
+import time
+
 from dynamixel_control import DynamixelController
 from robotic_arm_control import RoboticController
 
-arm_id_list = [11, 13, 15, 14, 12, 1, 2]
+id_list = [11, 13, 15, 14, 12, 1, 2]
 
 Dy = DynamixelController()
 Ro = RoboticController()
 Ro.open_robotic_arm("COM4", id_list, Dy)
+print("Arm init")
 
-grip_id = arm_id_list[-1]
-target_angle = 90
+grip_id = id_list[-1]
+target_angle = angle = 90
 dt = 0.1
+
+print("Opening")
 Dy.‎goal_absolute_direction(grip_id, target_angle)
+time.sleep(3)
+
+print("Start")
 while target_angle > 5:
 
     dangle = abs(angle - Dy.‎present_position(grip_id))
@@ -19,7 +27,10 @@ while target_angle > 5:
     angle_speed = dangle / dt
     print(angle_speed)
 
-    target_angle -= 5
+    target_angle -= 2
     Dy.‎goal_absolute_direction(grip_id, target_angle)
+
+    if angle_speed < 8.0:
+        print(f"Stop at {Dy.‎present_position(grip_id)}")
 
 Dy.‎goal_absolute_direction(grip_id, 90)
