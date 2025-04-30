24573a7a2e4307f0954e5c9f65bc4ff8fd4a24a7
Fambot_RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2025-04-30 10:28:08 +0900

    Update flexible_grip.py

--- flexible_grip.py
+++ flexible_grip.py
@@ -7,31 +7,43 @@ id_list = [11, 13, 15, 14, 12, 1, 2]
 
 Dy = DynamixelController()
 Ro = RoboticController()
-Ro.open_robotic_arm("COM4", id_list, Dy)
+Ro.open_robotic_arm("/dev/arm", id_list, Dy)
+
 print("Arm init")
 
 grip_id = id_list[-1]
-target_angle = angle = 90
-dt = 0.1
+target_angle = 90
+last_angle = Dy.present_position(grip_id)
+dt = 0.2
+final_angle = 0
+i = 0
 
 print("Opening")
-Dy.‎goal_absolute_direction(grip_id, target_angle)
-time.sleep(3)
+Ro.go_to_real_xyz_alpha(id_list, [0, 300, 150], 0, 0, target_angle, 0, Dy)
+Dy.profile_velocity(grip_id, 20)
 
 print("Start")
-while target_angle > 5:
+while target_angle > 0:
 
-    dangle = abs(angle - Dy.‎present_position(grip_id))
+    i += 1
+    angle = Dy.present_position(grip_id)
+    dangle = abs( last_angle - Dy.present_position(grip_id) )
     time.sleep(dt)
-    angle = Dy.‎present_position(grip_id)
+    last_angle = angle
+
+    angle = Dy.present_position(grip_id)
     angle_speed = dangle / dt
-    print(angle_speed)
+    print(angle, last_angle, angle_speed, target_angle, i)
 
-    target_angle = angle - 5
-    Dy.‎goal_absolute_direction(grip_id, target_angle)
+    target_angle = angle - 10
+    Dy.goal_absolute_direction(grip_id, max(target_angle, -5))
 
-    if angle_speed < 8.0:
-        print(f"Stop at {Dy.‎present_position(grip_id)}")
+    if angle_speed <= 20.0 and i > 3:
+        print(f"Stop at {Dy.present_position(grip_id)}")
+        final_angle = Dy.present_position(grip_id) + 4
+        Dy.goal_absolute_direction(grip_id, final_angle)
         break
 
-Dy.‎goal_absolute_direction(grip_id, 90)
+time.sleep(1)
+Ro.go_to_real_xyz_alpha(id_list, [0, 250, 150], -25, 0, final_angle, 0, Dy)
+
