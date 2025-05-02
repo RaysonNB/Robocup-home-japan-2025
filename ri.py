f97ff14d946819463cb37e8330f797e1d6e9fa9f
IsaacMak <95964568+IsaacMak2009@users.noreply.github.com>
2025-05-02 11:32:42 +0800

    Update ri.py

--- ri.py
+++ ri.py
@@ -9,10 +9,13 @@ from RobotChassis import RobotChassis
 
 POINT_DINING_ROOM = (-0.913,0.257,0.001) ##
 
+
+
 def main():
     clear_costmaps = rospy.ServiceProxy("/move_base/clear_costmaps", Empty)
     chassis_move = Chassis()
     chassis = RobotChassis()
+
     # navigator = Navigator()
     respeaker = Respeaker(enable_espeak_fix=True)
     cam1 = Camera("/cam2/color/image_raw", "bgr8")
@@ -21,6 +24,25 @@ def main():
     cx, cy = width // 2, height // 2
     rate = rospy.Rate(20)
 
+    def walk_to(point):
+        chassis.move_to(*point)
+        while not rospy.is_shutdown():
+            # 4. Get the chassis status.
+            rate = rospy.Rate(20)
+            code = chassis.status_code
+            text = chassis.status_text
+            logger.debug(f"Nav: {text}")
+            rate.sleep()
+            
+            if code == 3:
+                break
+            if code == 4:
+                logger.error("Fail to get a plan")
+                respeaker.say("Fail to get a plan")
+                clear_costmaps
+                chassis.move_to(*point)
+        return
+    
     while not rospy.is_shutdown():
         frame = cam1.get_frame()
         depth_frame = cam2.get_frame()
@@ -45,19 +67,13 @@ def main():
 
 
     # navigate
-    chassis.move_to(*POINT_DINING_ROOM)
-    while not rospy.is_shutdown():
-        # 4. Get the chassis status.
-        rate = rospy.Rate(20)
-        code = chassis.status_code
-        text = chassis.status_text
-        logger.debug(f"Nav: {text}")
-        if code == 3:
-            break
-
     clear_costmaps
+    walk_to([])
+    clear_costmaps
+    walk_to([])
+    clear_costmaps
+    walk_to([])
 
-    
 
 if __name__ == '__main__':
     rospy.init_node('test_camera', anonymous=True)
