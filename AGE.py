a57122bab30819aef03eb49af2a6e23590aa30c0
IsaacMak <95964568+IsaacMak2009@users.noreply.github.com>
2025-05-04 08:54:12 +0800

    Update AGE.py

--- AGE.py
+++ AGE.py
@@ -1,79 +1,9 @@
-import cv2
-import numpy as np
-import argparse
-
-#https://drive.google.com/drive/folders/16qqswNHvUCGQI4iCekXdd6T_-ePKZrzz
-# Add argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument('--image', help='Path to image file')
-parser.add_argument('--video', help='Path to video file', default=0)
-args = parser.parse_args()
-
-
-def highlightFace(net, frame, conf_threshold=0.7):
-    frameOpencvDnn = frame.copy()
-    frameHeight = frameOpencvDnn.shape[0]
-    frameWidth = frameOpencvDnn.shape[1]
-    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
-
-    net.setInput(blob)
-    detections = net.forward()
-    faceBoxes = []
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-
-        if confidence > conf_threshold:
-            x1 = int(detections[0, 0, i, 3] * frameWidth)
-            y1 = int(detections[0, 0, i, 4] * frameHeight)
-            x2 = int(detections[0, 0, i, 5] * frameWidth)
-            y2 = int(detections[0, 0, i, 6] * frameHeight)
-            faceBoxes.append([x1, y1, x2, y2])
-            cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8)
-    return frameOpencvDnn, faceBoxes
-
-
-# Load pre-trained models
-faceProto = "opencv_face_detector.pbtxt"
-faceModel = "opencv_face_detector_uint8.pb"
-ageProto = "age_deploy.prototxt"
-ageModel = "age_net.caffemodel"
-
-faceNet = cv2.dnn.readNet(faceModel, faceProto)
-ageNet = cv2.dnn.readNet(ageModel, ageProto)
-
-# Age categories
-ageList = ['1', '5', '10', '17', '27', '41', '50', '67']
-
-# Initialize video capture
-video = cv2.VideoCapture(args.video if args.video else 0)
-padding = 20
-
-while cv2.waitKey(1) < 0:
-    hasFrame, frame = video.read()
-    if not hasFrame:
-        cv2.waitKey()
-        break
-
-    resultImg, faceBoxes = highlightFace(faceNet, frame)
-
-    if not faceBoxes:
-        print("No face detected")
-        continue
-
-    for faceBox in faceBoxes:
-        face = frame[max(0, faceBox[1] - padding):
-                     min(faceBox[3] + padding, frame.shape[0] - 1),
-               max(0, faceBox[0] - padding):
-               min(faceBox[2] + padding, frame.shape[1] - 1)]
-
-        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
-
-        ageNet.setInput(blob)
-        agePreds = ageNet.forward()
-        age = ageList[agePreds[0].argmax()]
-        print(f'Age: {age[1:-1]} years')
-
-        cv2.putText(resultImg, f'Age: {age}', (faceBox[0], faceBox[1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
-
-    cv2.imshow("Detecting age", resultImg)
+- Say your team's name to the person pointing to the left in the bedroom
+- Give me a cup from the shelf
+- Meet Yoshimura in the study room and escort them to the tall table
+- Navigate to the study room then meet Andrew and follow them to the dining room
+- Meet Jack at the right tray then locate them in the study room
+- Answer the quiz of the person giving the V sign in the study room
+- Answer the question of the person pointing to the left in the living room
+- Tell me what is the thinnest object on the low table
+- Tell me what is the heaviest object on the shelf
